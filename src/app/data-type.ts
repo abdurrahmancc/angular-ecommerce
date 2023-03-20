7e8c0c3796cc548e117be1255c8916863066adbd
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2023-03-21 03:32:44 +0600

    added product form api

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -7,3 +7,13 @@ export interface SignIn {
   email: string
   password: string
 }
+
+export interface Product {
+  name: string
+  price: number
+  category: string
+  color: string
+  image: string
+  description: string
+  id?: string
+}
--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Product } from '../data-type'
+import { ProductService } from '../services/product.service'
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent {
+  addProductMessage: string | undefined
+  constructor (private product: ProductService) {}
 
+  submitProduct (data: Product) {
+    this.product.addProduct(data).subscribe((res: any) => {
+      if (res) {
+        this.addProductMessage = 'Product is added successfully'
+      }
+    })
+    setTimeout(() => {
+      this.addProductMessage = undefined
+    }, 3000)
+  }
 }
